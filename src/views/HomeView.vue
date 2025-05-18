f981ba21e677f428f7bc50c0bb3502a6424fcab4
Yudai Fujieda <Yudai.Fujieda@ibm.com>
2025-05-18 15:56:37 +0900

    home修正

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <img src="https://www.myinstants.com/media/instants_images/nakayama-kinnikun-power.jpg" alt="kinniku logo">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
